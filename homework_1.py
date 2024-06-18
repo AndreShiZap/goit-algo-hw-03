772f7ef991d359a578f5ab99c040ca4913978ef0
AndreShiZap <andreshizap@gmail.com>
2024-06-18 14:49:21 +0300

    commit-1806_2

--- homework_1.py
+++ homework_1.py
@@ -22,12 +22,17 @@ def get_numbers_ticket(min, max, quantity):
     # у разі невідповідності повертається порожній список
     if not ((1 <= min <= 1000) and (1 <= max <= 1000) and (min < max) and (0 < quantity <= (max - min))):
         return(numbers_ticket)
-    i = 1
-    while i <= quantity:
-        number = random.randint(min, max)       # отримання випадкового числа
-        if numbers_ticket.count(number) == 0:   # перевірка на унікальність числа серед уже одержаних
-            numbers_ticket.append(number)       # додавання унікального випадкового числа
-            i += 1
+    d_numbers_ticket = set(numbers_ticket)              # перетворення списку у множину 
+    while len(d_numbers_ticket) < quantity:             # перевірка кількості чисел у множині
+        d_numbers_ticket.add(random.randint(min, max))  # додавання випадкового числа у множину
+    numbers_ticket = list(d_numbers_ticket)             # перетворення множини у список
+    # другий варіант - без використання множини:    
+    #i = 1
+    #while i <= quantity:
+    #    number = random.randint(min, max)       # отримання випадкового числа
+    #    if numbers_ticket.count(number) == 0:   # перевірка на унікальність числа серед уже одержаних
+    #        numbers_ticket.append(number)       # додавання унікального випадкового числа
+    #        i += 1
     numbers_ticket.sort()                       # сортировка вихідного списку
     return(numbers_ticket)
 
